155887d83b43655bd9d0cf4483caa1ef9740c9e9
Sonu Kumar Paswan <sonupaswan0381@gmail.com>
2025-03-19 21:03:25 +0530

    update

--- 2D-Array/Cre.java
+++
@@ -1,5 +0,0 @@
-public class Creaction{
-    public static void main(String[] args) {
-        
-    }
-}
--- 2D-Array/Creaction.java
+++ 2D-Array/Creaction.java
@@ -1,1 +1,23 @@
-public class Creaction
+import java.util.Scanner;
+
+public class Creaction {
+    public static void main(String[] args) {
+        int[][] martrix = new int[3][3];
+        int n = martrix.length;
+        int m = martrix[0].length;
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                martrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(martrix[i][j] + "  ");
+            }
+            System.out.println();
+        }
+
+    }
+}
