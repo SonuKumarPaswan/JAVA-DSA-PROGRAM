22936d65c24da24fe1aef3a8b53be4c2b1b8dab0
Sonu Kumar Paswan <sonupaswan0381@gmail.com>
2025-03-22 22:54:14 +0530

    update

---
+++ Bit-Manipulation/BitWiseOperator.java
@@ -0,0 +1,7 @@
+public class BitWiseOperator {
+    public static void main(String[] args) {
+        System.out.println(5 & 4);
+        System.out.println(5 | 4);
+        System.out.println(5 ^ 4);
+    }
+}
